b1d1caf68cef8c5c03bc648e51fc89720976fec4
LuisAPI <imperialluisanton@outlook.ph>
2025-12-17 18:05:42 +0800

    test(frontend): improve react-router-dom mock to render Route elements

--- frontend/src/__mocks__/react-router-dom.js
+++ frontend/src/__mocks__/react-router-dom.js
@@ -3,7 +3,7 @@ const React = require('react');
 module.exports = {
   BrowserRouter: ({ children }) => React.createElement(React.Fragment, null, children),
   Routes: ({ children }) => React.createElement(React.Fragment, null, children),
-  Route: () => null,
+  Route: ({ element }) => (typeof element === 'function' ? React.createElement(element) : element || null),
   Link: ({ children }) => React.createElement('a', null, children),
   NavLink: ({ children }) => React.createElement('a', null, children),
   useNavigate: () => () => {},
